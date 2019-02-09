12241ffb5f58bb84b7ba36fee8b00641baa048aa
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-09 11:42:22 -0700

    completed carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.images = Array.from(this.element.querySelectorAll("img"));
+    this.currentImage = 0;
 
+    console.log(this.currentImage);
+
+    console.log(this.images[this.currentImage]);
+
+    this.images[this.currentImage].style.display = "inline-block";
+
+    let leftNav = this.element.querySelector(".left-button");
+    let rightNav = this.element.querySelector(".right-button");
+
+    leftNav.addEventListener("click", e => this.showImage("left"));
+    rightNav.addEventListener("click", e => this.showImage("right"));
+  }
+
+  showImage(direction) {
+    this.images[this.currentImage].style.display = "none";
+
+    switch (direction) {
+      case "left":
+        this.currentImage--;
+        break;
+      case "right":
+        this.currentImage++;
+        break;
+      default:
+    }
+
+    if (this.currentImage <= 0) this.currentImage = this.images.length - 1;
+    else if (this.currentImage >= this.images.length - 1) this.currentImage = 0;
+
+    this.images[this.currentImage].style.display = "inline-block";
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector(".carousel");
+console.log(carousel);
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
